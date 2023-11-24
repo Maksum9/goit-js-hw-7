f5bb0afc9b5fa70e2564d3261740966676bdfbfb
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-11-24 19:52:40 +0200

    up

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.getElementById("categories");
+const categoryItem = categories.querySelector(`li.item`);
+console.log(`Number of categories : ${categories}`);
+categoryItem.forEach((categoryItem) => {
+const categoryTitle = categoryItem.querySelector(`ul > li`).length;
+console.log(`Category: ${categoryTitle}`);
+console.log(`Number of items in this category: ${categoryItemsCount}`);
+})
+
