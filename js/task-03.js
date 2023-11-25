4fabc461b02208ef04aa6010a2e6038574f04c35
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-11-25 14:49:02 +0200

    update

---
+++ js/task-03.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector('#name-input');
+const nameInput = document.querySelector('#mane-output');
+textInput = nameInput.strim();
+if (textInput = '') {
+    nameInput = "Anonymous";
+}
