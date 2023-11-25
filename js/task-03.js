137154b2f035ccb96c41ef1779d759bb93b78559
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-11-25 15:52:20 +0200

    up

--- js/task-03.js
+++ js/task-03.js
@@ -1,5 +1,5 @@
 const textInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#mane-output');
+const nameOutput = document.querySelector('#name-output');
 textInput.addEventListener('input',function() {
 const trimmedInput = textInput.value.trim();
 if (trimmedInput === '') {
