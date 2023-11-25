95479ce273c04784df5a1232052dc96fcd12d420
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-11-25 15:05:46 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#mane-output');
 textInput.addEventListener('input',function() {
-const trimmedInput = textInput.trim();
+const trimmedInput = textInput.value.trim();
 if (trimmedInput === '') {
     nameOutput.textContent = "Anonymous";
 } else {
