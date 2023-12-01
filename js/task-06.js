97a55c39b955013841975bfcec36d14a82c83a91
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-12-01 16:42:39 +0200

    up

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
 const box = document.querySelector("#boxes");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const input = document.querySelector("input");
 let arrEl = [];
 let num = 0;
-// function create element and add element in box
+let sizeIncrement = 10;
+
 const createElement = (n) => {
   for (let i = 0; i < n; i++) {
-    const newEl = <div style="background-color: ${getRandomHexColor()}; width: 30px; height: 30px"></div>;
+    const newSize = 30 + i * sizeIncrement;
+    const newEl = `<span style="background-color: ${getRandomHexColor()}; width: ${newSize}px; height: ${newSize}px; display: inline-block; margin: 5px;"></span>`;
     arrEl.push(newEl);
   }
-  box.insertAdjacentHTML("afterbegin", arrEl.join(""));
+  box.insertAdjacentHTML("beforeend", arrEl.join(""));
 };
-//get input value
+
+const destroyElements = () => {
+  box.innerHTML = "";
+};
+
 input.addEventListener("blur", () => {
-  createBtn.addEventListener("click", createElement(input.value));
+  createBtn.addEventListener("click", () => {
+    createElement(input.value);
+  });
 });
+
+destroyBtn.addEventListener("click", destroyElements);
