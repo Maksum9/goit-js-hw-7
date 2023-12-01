4e0b3d7db4ded577c115d34cdaf5c96cbab73bde
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-12-01 12:57:15 +0200

    update

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const box = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+let arrEl = [];
+let num = 0;
+// function create element and add element in box
+const createElement = (n) => {
+  for (let i = 0; i < n; i++) {
+    const newEl = <div style="background-color: ${getRandomHexColor()}; width: 30px; height: 30px"></div>;
+    arrEl.push(newEl);
+  }
+  box.insertAdjacentHTML("afterbegin", arrEl.join(""));
+};
+//get input value
+input.addEventListener("blur", () => {
+  createBtn.addEventListener("click", createElement(input.value));
+});
